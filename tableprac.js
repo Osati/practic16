14774522ce18ec648358a648593cbf05529f04d5
Aniscs452 <106110762+Osati@users.noreply.github.com>
2022-08-13 19:14:46 +0600

    Update tableprac.js

--- tableprac.js
+++ tableprac.js
@@ -1,14 +1,14 @@
-var fname,lname,email,table_ref,insert_row,fname_col,lname_col,email_col,action_col;
+var fname,lname,Email,table_ref,insert_row,fname_col,lname_col,email_col,action_col;
 
 function createNewData(){
-   fname = document.getElementById("fname").Value;
-   lname = document.getElementById("lname").Value;
-   email = document.getElementById("email").Value;
+   fname = document.getElementById("fnam").Value;
+   lname = document.getElementById("lnam").Value;
+   Email = document.getElementById("email").Value;
    table_ref =document.getElementById("info_table").getElementsByTagName("tbody")[0];
    insert_row = table_ref.insertRow(table_ref.rows.length); 
    fname_col = insert_row.insertCell(0).innerHTML = fname;
    lname_col = insert_row.insertCell(1).innerHTML = lname;
-   email_col = insert_row.insertCell(2).innerHTML = email;
+   email_col = insert_row.insertCell(2).innerHTML = Email;
    action_col = insert_row.insertCell(3).innerHTML = '<td><a class="btn btn-success" >Edit</a><a class="btn btn-danger" >Delete</a></td>';
    
    
