bfa781a5c5949610149d19b5986e6d677bd16e4a
Osati <aniscs452@gmail.com>
2022-08-13 19:42:26 +0600

    this project is change

--- tableprac.js
+++ tableprac.js
@@ -1,10 +1,10 @@
 var fname,lname,email,table_ref,insert_row,fname_col,lname_col,email_col,action_col;
 
 function createNewData(){
-   fname = document.getElementById("fname").Value;
-   lname = document.getElementById("lname").Value;
-   email = document.getElementById("email").Value;
-   table_ref =document.getElementById("info_table").getElementsByTagName("tbody")[0];
+   fname = document.getElementById("fname").value;
+   lname = document.getElementById("lname").value;
+   email = document.getElementById("email").value;
+   table_ref = document.getElementById("info_table").getElementsByTagName("tbody")[0];
    insert_row = table_ref.insertRow(table_ref.rows.length); 
    fname_col = insert_row.insertCell(0).innerHTML = fname;
    lname_col = insert_row.insertCell(1).innerHTML = lname;
